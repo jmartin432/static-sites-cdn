4959e3ba4cf7a5254a3d4a59f4292b1211f3f144
Justin Martin <jlmartin432@gmail.com>
2019-11-09 20:19:00 -0800

    added portfolio to stack

--- lambdas/originRouter.js
+++ lambdas/originRouter.js
@@ -10,19 +10,31 @@ exports.handler = (event, context, callback) => {
     const peaksValleys = 'peaks-valleys';
     const creativeCoding = 'creative-coding';
     const hangman = 'hangman';
+    const sunriseSunset = 'sunrise';
+    const blackjack = 'blackjack';
+    const portfolio = 'portfolio';
 
     if (routingUrl.startsWith(testSite)) {
         console.log('Routing to test-site');
-        request.origin.s3.path = `/${testSite}`
+        request.origin.s3.path = `/${testSite}`;
     } else if (routingUrl.startsWith(peaksValleys)) {
         console.log('Routing to peaks-valleys');
-        request.origin.s3.path = `/${peaksValleys}`
+        request.origin.s3.path = `/${peaksValleys}`;
     } else if (routingUrl.startsWith(creativeCoding)) {
         console.log('Routing to creative-coding');
-        request.origin.s3.path = `/${creativeCoding}`
+        request.origin.s3.path = `/${creativeCoding}`;
     } else if (routingUrl.startsWith(hangman)) {
         console.log('Routing to hangman');
-        request.origin.s3.path = `/${hangman}`
+        request.origin.s3.path = `/${hangman}`;
+    } else if (routingUrl.startsWith(sunriseSunset)) {
+        console.log('Routing to sunrise-sunset');
+        request.origin.s3.path = `/${sunriseSunset}`
+    } else if (routingUrl.startsWith(blackjack)) {
+        console.log('Routing to blackjack');
+        request.origin.s3.path = `/${blackjack}`;
+    } else if (routingUrl.startsWith(portfolio)) {
+        console.log('Routing to blackjack');
+        request.origin.s3.path = `/${portfolio}`;
     } else {
         console.log('Routing to default root object as Default');
     }
--- test/originRouterTest.js
+++ test/originRouterTest.js
@@ -63,6 +63,52 @@ it('Test hangman-dev', () => {
     });
 });
 
+it('Test sunrise-dev', () => {
+    event.Records[0].cf.request.headers.host[0].value = "sunrise-dev.justinlmartin.com";
+    event.Records[0].cf.request.origin.s3.path = '/';
+    originRouter.handler(event, {}, function(err, result) {
+      if (err)
+        console.log(err);
+      if (result)
+        console.log("Result: " + JSON.stringify(result, null, 2));
+        expect(result.origin.s3.path).to.equal('/sunrise');
+        expect(result.origin.s3.domainName).to.equal('static-sites-bucket.s3.amazonaws.com');
+        expect(result.headers.host[0].value).to.equal('static-sites-bucket.s3.amazonaws.com');
+
+    });
+});
+
+it('Test blackjack-dev', () => {
+    event.Records[0].cf.request.headers.host[0].value = "blackjack-dev.justinlmartin.com";
+    event.Records[0].cf.request.origin.s3.path = '/';
+    originRouter.handler(event, {}, function(err, result) {
+      if (err)
+        console.log(err);
+      if (result)
+        console.log("Result: " + JSON.stringify(result, null, 2));
+        expect(result.origin.s3.path).to.equal('/blackjack');
+        expect(result.origin.s3.domainName).to.equal('static-sites-bucket.s3.amazonaws.com');
+        expect(result.headers.host[0].value).to.equal('static-sites-bucket.s3.amazonaws.com');
+
+    });
+});
+
+it('Test portfolio-dev', () => {
+    event.Records[0].cf.request.headers.host[0].value = "portfolio-dev.justinlmartin.com";
+    event.Records[0].cf.request.origin.s3.path = '/';
+    originRouter.handler(event, {}, function(err, result) {
+      if (err)
+        console.log(err);
+      if (result)
+        console.log("Result: " + JSON.stringify(result, null, 2));
+        expect(result.origin.s3.path).to.equal('/portfolio');
+        expect(result.origin.s3.domainName).to.equal('static-sites-bucket.s3.amazonaws.com');
+        expect(result.headers.host[0].value).to.equal('static-sites-bucket.s3.amazonaws.com');
+
+    });
+});
+
+
 // Prod Tests...
 
 it('Test test-site-prod', () => {
@@ -119,6 +165,51 @@ it('Test hangman-prod', () => {
       if (result)
         console.log("Result: " + JSON.stringify(result, null, 2));
         expect(result.origin.s3.path).to.equal('/hangman');
+        expect(result.origin.s3.domainName).to.equal('static-sites-bucket.s3.amazonaws.com');
+        expect(result.headers.host[0].value).to.equal('static-sites-bucket.s3.amazonaws.com');
+
+    });
+});
+
+it('Test sunrise-prod', () => {
+    event.Records[0].cf.request.headers.host[0].value = "sunrise.justinlmartin.com";
+    event.Records[0].cf.request.origin.s3.path = '/';
+    originRouter.handler(event, {}, function(err, result) {
+      if (err)
+        console.log(err);
+      if (result)
+        console.log("Result: " + JSON.stringify(result, null, 2));
+        expect(result.origin.s3.path).to.equal('/sunrise');
+        expect(result.origin.s3.domainName).to.equal('static-sites-bucket.s3.amazonaws.com');
+        expect(result.headers.host[0].value).to.equal('static-sites-bucket.s3.amazonaws.com');
+
+    });
+});
+
+it('Test blackjack-prod', () => {
+    event.Records[0].cf.request.headers.host[0].value = "blackjack.justinlmartin.com";
+    event.Records[0].cf.request.origin.s3.path = '/';
+    originRouter.handler(event, {}, function(err, result) {
+      if (err)
+        console.log(err);
+      if (result)
+        console.log("Result: " + JSON.stringify(result, null, 2));
+        expect(result.origin.s3.path).to.equal('/blackjack');
+        expect(result.origin.s3.domainName).to.equal('static-sites-bucket.s3.amazonaws.com');
+        expect(result.headers.host[0].value).to.equal('static-sites-bucket.s3.amazonaws.com');
+
+    });
+});
+
+it('Test portfolio-prod', () => {
+    event.Records[0].cf.request.headers.host[0].value = "portfolio.justinlmartin.com";
+    event.Records[0].cf.request.origin.s3.path = '/';
+    originRouter.handler(event, {}, function(err, result) {
+      if (err)
+        console.log(err);
+      if (result)
+        console.log("Result: " + JSON.stringify(result, null, 2));
+        expect(result.origin.s3.path).to.equal('/portfolio');
         expect(result.origin.s3.domainName).to.equal('static-sites-bucket.s3.amazonaws.com');
         expect(result.headers.host[0].value).to.equal('static-sites-bucket.s3.amazonaws.com');
 
