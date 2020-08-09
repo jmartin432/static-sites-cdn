6e850e8798b38814f6d78f9d552fb38e971c8770
Justin <info@justinlmartin.com>
2020-08-08 23:39:39 -0700

    adding custom resource for lambda versioning

--- lambdas/origin-router-lambda.js
+++ lambdas/origin-router-lambda.js
@@ -38,51 +38,5 @@ exports.handler = (event, context, callback) => {
 
     request.headers['host'] = [{ key: 'Host', value: bucketDomain}];
     console.log('Updated request to:' + JSON.stringify(request));
-    callback(null, request);
-
-    // const testSite = 'test-site';
-    // const peaksValleys = 'peaks-valleys';
-    // const creativeCoding = 'creative-coding';
-    // const spaceblobman = 'spaceblobman';
-    // const sunriseSunset = 'sunrise-sunset';
-    // const blackjack = 'blackjack';
-    // const portfolio = 'portfolio';
-    // const sunflowerSynth = 'sunflower-synth'
-    // const sampampulancer = 'sampampulancer'
-    // const www = 'www';
-    // const topLevel = 'justin';
-    //
-    // if (routingUrl.startsWith(testSite)) {
-    //     console.log('Routing to test-site');
-    //     request.origin.s3.path = `/${testSite}`;
-    // } else if (routingUrl.startsWith(peaksValleys)) {
-    //     console.log('Routing to peaks-valleys');
-    //     request.origin.s3.path = `/${peaksValleys}`;
-    // } else if (routingUrl.startsWith(creativeCoding)) {
-    //     console.log('Routing to creative-coding');
-    //     request.origin.s3.path = `/${creativeCoding}`;
-    // } else if (routingUrl.startsWith(spaceblobman)) {
-    //     console.log('Routing to spaceblobman');
-    //     request.origin.s3.path = `/${spaceblobman}`;
-    // } else if (routingUrl.startsWith(sunriseSunset)) {
-    //     console.log('Routing to sunrise-sunset');
-    //     request.origin.s3.path = `/${sunriseSunset}`
-    // } else if (routingUrl.startsWith(blackjack)) {
-    //     console.log('Routing to blackjack');
-    //     request.origin.s3.path = `/${blackjack}`;
-    // } else if (routingUrl.startsWith(sunflowerSynth)) {
-    //     console.log('Routing to sunflower-synth');
-    //     request.origin.s3.path = `/${sunflowerSynth}`;
-    // } else if (routingUrl.startsWith(sampampulancer)) {
-    //     console.log('Routing to sampampulancer');
-    //     request.origin.s3.path = `/${sampampulancer}`;
-    // }else if (routingUrl.startsWith(portfolio) || routingUrl.startsWith(www) || routingUrl.startsWith(topLevel)){
-    //     console.log('Routing to portfolio');
-    //     request.origin.s3.path = `/${portfolio}`;
-    // } else {
-    //     console.log('Routing to default root object as Default');
-    // }
-    // request.headers['host'] = [{ key: 'Host', value: bucketDomain}];
-    // console.log('Updated Request ' + JSON.stringify(request));
-    // callback(null, request);
+    callback(null, request)
 }
--- lambdas/version-publisher-lambda.py
+++ lambdas/version-publisher-lambda.py
@@ -2,9 +2,7 @@ import boto3
 import time
 import logging
 import json
-from string import Template
-import os
-import datetime
+import requests
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -12,15 +10,73 @@ logger.setLevel(logging.INFO)
 
 def handler(event, context):
     logger.info(event)
-    client = boto3.client('lambda')
-    response1 = client.list_versions_by_fuction(
-        FunctionName='static-sites-cdn-dev-origin-router'
-    )
-    logger.info('Response1: ', response1)
-    response2 = client.list_versions_by_fuction(
-        FunctionName='static-sites-cdn-dev-custom-headers'
-    )
-    logger.info('Response2: ', response2)
+    try:
+        request_type = event.get('RequestType', None)
+        resource_type = event.get('ResourceType', None)
+        response_url = event.get('ResponseURL', None)
+        request_properties = event.get('ResourceProperties', None)
+        function_names = request_properties['FunctionNames']
 
+        response_data = {
+            'StackId': event.get('StackId', None),
+            'RequestId': event.get('RequestId', None),
+            'LogicalResourceId': event.get('LogicalResourceId', ""),
+            'PhysicalResourceId': event.get('PhysicalResourceId', '{name}-{version}'
+                                            .format(name=context.function_name, version=context.function_version)),
+            'Data': {},
+            'Reason': 'Check CloudWatch'
+        }
+    except Exception:
+        logger.exception('There was an error reading event data:')
+        return {}
+    try:
+        client = boto3.client('lambda')
+    except Exception:
+        logger.exception('There was an error creating Lambda Client:')
+        response_data['Status'] = 'FAILED'
+        return {response_data}
 
-    return
+    for key in function_names:
+        try:
+            logger.info('Publishing version for function: {name}'.format(name=function_names[key]))
+            publish_response = client.publish_version(
+                FunctionName=function_names[key]
+            )
+            logger.info('Publish version response: {response}'.format(response=publish_response))
+            if publish_response['ResponseMetadata']['HTTPStatusCode'] != 201:
+                logger.error('Publish Version FAILED: HTTP Status Code: {status_code}'
+                             .format(status_code=publish_response['ResponseMetadata']['HTTPStatusCode']))
+                response_data['Status'] = 'FAILED'
+            else:
+                response_data['Status'] = 'SUCCESS'
+                new_key = key + 'QualifiedArn'
+                response_data['Data'][new_key] = publish_response['FunctionArn']
+                logger.info('Qualified function ARN: {arn}'.format(arn=publish_response['FunctionArn']))
+        except Exception:
+            response_data['Status'] = 'FAILED'
+            logger.exception('There was an error sending publish version request, returning: {response}'
+                             .format(response=response_data))
+            return response_data
+
+    logger.info('Ready to send data: {data}'.format(data=response_data['Data']))
+
+    json_response_data = json.dumps(response_data)
+    headers = {
+        'content-type': '',
+        'content-length': str(len(json_response_data))
+    }
+    logger.info('Sending response to S3 for CloudFormation: {response}'.format(response=json_response_data))
+    try:
+        s3_response = requests.put(
+            response_url,
+            data=json_response_data,
+            headers=headers)
+        logger.info('S3 returned status code: {response}'.format(response=s3_response))
+    except Exception:
+        response_data['Status'] = 'FAILED'
+        logger.exception('There was an error sending response to S3 Bucket, returning: {response}'
+                         .format(response=response_data))
+        return response_data
+
+    return response_data
+
