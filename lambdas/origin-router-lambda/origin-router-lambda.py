0a32e93434c96dc309f9f502fd4d377cb9b8d7f7
Justin <info@justinlmartin.com>
2021-07-02 00:15:31 -0700

    adding record fro unicorn warp

--- lambdas/origin-router-lambda/origin-router-lambda.py
+++ lambdas/origin-router-lambda/origin-router-lambda.py
@@ -22,38 +22,25 @@ def handler(event, context):
         'blackjack',
         'sunflower-synth',
         'sampampulancer',
-        'coding-portfolio-dev',
-        'qr-dev',
-        'landing-page-dev',
-        'test-site-dev',
-        'peaks-valleys-dev',
-        'creative-coding-dev',
-        'spaceblobman-dev',
-        'sunrise-sunset-dev',
-        'blackjack-dev',
-        'sunflower-synth-dev',
-        'sampampulancer-dev'
+        'unicorn-warp'
     ]
 
     request = event['Records'][0]['cf']['request']
     bucket_domain = event['Records'][0]['cf']['request']['origin']['s3']['domainName']
     routing_url = event['Records'][0]['cf']['request']['headers']['host'][0]['value']
     subdomain = routing_url.split('.')[0]
-    # site = subdomain.replace('-dev', '')
+    site = subdomain.replace('-dev', '')
 
-    logger.info('Routing: {url}, {subdomain}'.format(url=routing_url, subdomain=subdomain))
+    logger.info('Routing: {url}, {subdomain}, {site}'.format(url=routing_url, subdomain=subdomain, site=site))
 
-    if subdomain not in keys_list:
-        logger.error('Incorrect subdomain. Re-Routing to default root object.')
-    elif subdomain == 'www' or subdomain == 'justinlmartin' or subdomain == 'qr':
-        logger.info('Re-Routing to prod landing page')
+    if site not in keys_list:
+        logger.error('Incorrect subdomain. Routing to default root object.')
+    elif site == 'www' or site == 'justinlmartin' or site == 'qr':
+        logger.info('Re-Routing to landing page')
         request['origin']['s3']['path'] = '/landing-page'
-    elif subdomain == 'qr-dev':
-        logger.info('Re-Routing to dev landing page')
-        request['origin']['s3']['path'] = '/landing-page-dev'
     else:
-        logger.info('Routing to {subdomain}'.format(subdomain=subdomain))
-        request['origin']['s3']['path'] = '/{subdomain}'.format(subdomain=subdomain)
+        logger.info('Routing to {site}'.format(site=site))
+        request['origin']['s3']['path'] = '/{site}'.format(site=site)
 
     request['headers']['host'] = [{
         'key': 'Host',
